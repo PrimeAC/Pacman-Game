0817b0dba09f66ebb561425f4c475d0ece1777de
PrimeAC <afonso.caetano@outlook.com>
2017-11-09 00:59:07 +0000

    Change Client side file name.

--- Client/Client.cs
+++ Client/Client.cs
@@ -6,7 +6,7 @@ using System.Windows.Forms;
 
 namespace Client
 {
-    static class Program
+    static class Client
     {
         /// <summary>
         /// The main entry point for the application.
